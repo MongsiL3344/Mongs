26fdda7312adb0a073bb8acec3d32d16613b71c4
mongsil <wkdgustjrrpdla@naver.com>
2025-04-03 17:22:33 +0900

    아이키퍼 C멘토링 1주차 과제

---
+++ Cmentoring/1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    int a;
+    float b,c;
+
+    a = 4;
+    b = 3.141592;
+    c = 15.1;
+
+    printf("a = %d\nb = %f\nc = %.1f", a, b, c);
+
+    return 0;
+}
